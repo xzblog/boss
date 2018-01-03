0ee98cbdc0d7614e7448ffc44590dbfa05e542eb
xzblog <1772176802@qq.com>
2018-01-03 23:30:42 +0800

    添加redux浏览器开发插件

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import {amount} from './reduce';
 import './index.css';
 import App from './App';
-const store = createStore(amount, applyMiddleware(thunk));
+
+
+const store = createStore(amount,compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+));
 function render(){
     ReactDOM.render(
         <App store = {store} />,
